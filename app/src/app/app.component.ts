498881587b30be93ca582950f95a20c851cbf774
Sergio  Hernandez <sergiohernandez@sergios-mbp.lsm.local>
2019-09-04 12:53:55 +0100

    angular setting to display wp with angular

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  posts = [];
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void{
+    this.http.get('http://localhost:8090/?rest_route=/wp/v2/posts/').subscribe ( data => {
+      for(let key in data){
+        if(data.hasOwnProperty(key)){
+          this.posts.push(data[key]);
+        }
+      }
+      console.log(this.posts);
+    })
+  }
+
   title = 'app';
 }
